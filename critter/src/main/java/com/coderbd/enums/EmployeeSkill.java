0fc12f9628825b4e4516a24bc49a5da6fcc0a1bf
Rajaul <rajaul.cse@gmail.com>
2020-11-29 21:30:57 +0600

    Remove project

--- critter/src/main/java/com/coderbd/enums/EmployeeSkill.java
+++
@@ -1,8 +0,0 @@
-package com.coderbd.enums;
-
-/**
- * A example list of employee skills that could be included on an employee or a schedule request.
- */
-public enum EmployeeSkill {
-    PETTING, WALKING, FEEDING, MEDICATING, SHAVING;
-}
